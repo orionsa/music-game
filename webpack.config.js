01d02fd1467cc7cbb30f0e031f94ed6283efb7d4
Orion Sasson <adikadosh@Yogevs-MacBook-Air.local>
2018-02-10 22:39:07 +0200

    cleared code

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
   module: {
     rules: [
       { test:/\.js$/, use: 'babel-loader' , exclude: /node_modules/ },
-     // { test:/\.css$/, use: ['style-loader','css-loader'] },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
